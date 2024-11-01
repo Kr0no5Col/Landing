ff0222f77cbf2b6db82b9bdcc6dac8c1472aaf62
Kr0no5Col <andresespitia67@gmail.com>
2024-11-01 22:13:37 +1100

    Fix error images

--- src/pages/ContactoPage.jsx
+++ src/pages/ContactoPage.jsx
@@ -4,10 +4,10 @@ export const ContactoPage = () => {
   return (
     <div id = "Contact" className='my-20'>
     <div className="flex flex-row gap-10 justify-center">
-    <a href="https://www.facebook.com/andres.espitia.9674/"><img src="./Images/LogoFacebook.png" alt="logo facebook" className="w-10" /></a>
-    <a href="https://www.instagram.com/andresespitia93/"><img src="./Images/LogoInstagram.png" alt="logo Ig" className="w-10"/></a> 
-    <a href="https://www.linkedin.com/in/andres-espitia-551931b8"><img src="./Images/LogoLinked.png" alt="logo Linkelind" className="w-10"/></a> 
-    <a href="https://github.com/Kr0no5Col"><img src="./Images/LogoGithuh.png" alt="logo facebook" className="w-10"/></a>  
+    <a href="https://www.facebook.com/andres.espitia.9674/"><img src="/Images/LogoFacebook.png" alt="logo facebook" className="w-10" /></a>
+    <a href="https://www.instagram.com/andresespitia93/"><img src="/Images/LogoInstagram.png" alt="logo Ig" className="w-10"/></a> 
+    <a href="https://www.linkedin.com/in/andres-espitia-551931b8"><img src="/Images/LogoLinked.png" alt="logo Linkelind" className="w-10"/></a> 
+    <a href="https://github.com/Kr0no5Col"><img src="/Images/LogoGithuh.png" alt="logo facebook" className="w-10"/></a>  
  
  
    </div>
--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -5,7 +5,7 @@ export const PerfilPage = () => {
     <>
       <div id="about" >
         <h1 className=" text-4xl text-center text-pink-400 font-bold">About me</h1>
-        <img src="./Images/Icon1.png" alt="icono" className="w-40 m-auto"/>
+        <img src="/Images/Icon1.png" alt="icono" className="w-40 m-auto"/>
         <h2 className="text-white text-center px-12 text-2xl">
           ICT Support Engineer, Support Analyst, experienced in Help Desk,
           Answer Desk in various companies and clients, providing support in
